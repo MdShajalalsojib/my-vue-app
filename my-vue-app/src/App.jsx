92c049a5bd6a00c11407f31198378d18ad7a0cfc
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-04-23 22:55:15 +0600

    first commit

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,36 @@
+ 
+import './App.css'
+
+function App() {
+
+  function handleClick(){
+    alert('I am clicked')
+  }
+  const handleClick3 = () => {
+    alert('clicked this number 3')
+  }
+
+  const handleAdds5 = (num) =>{
+    const newNum = num + 5;
+    alert(newNum);
+  }
+   
+
+  return (
+    <>
+      
+      <h1>Md.Shajalal</h1>
+
+      {/* javaScript
+      <button onclick = "handleClick()">Click me</button> */}
+      <button  onClick = {handleClick}>Click ME</button>
+      <button onClick = {handleClick3}>Click Me3</button>
+
+      <button onClick ={ () => handleAdds5(10)}>click add 5</button>
+       
+       
+    </>
+  )
+}
+
+export default App
